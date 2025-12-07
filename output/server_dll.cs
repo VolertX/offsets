7d5bcb7121d5c1e7b744160e49787fee840e50c4
VolertX <111454826+VolertX@users.noreply.github.com>
2025-12-07 22:51:10 +0400

    Add files via upload

--- output/server_dll.cs
+++ output/server_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-11-26 03:52:32.508299300 UTC
+// 2025-12-07 18:48:15.533045900 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: server.dll
